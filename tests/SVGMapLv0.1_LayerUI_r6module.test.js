f3c46253ede418b6b717a7b5ecadca0353d2fb35
kusariya <yusaki.4kusari@gmail.com>
2024-09-11 11:57:37 +0900

    setLayerSpecificWebAppLaunchUiEnableAPIの正常系試験を実施

--- tests/SVGMapLv0.1_LayerUI_r6module.test.js
+++ tests/SVGMapLv0.1_LayerUI_r6module.test.js
@@ -4,16 +4,24 @@ import {jest} from "@jest/globals";
 
 describe("unittest for LayerUI module",()=>{
     describe("target svgMapImagesProps class",()=>{
-        let svgmaplayerui;
+        let svgmaplayerui, closeButton;
         beforeEach(()=>{
             svgmaplayerui = new SvgMapLayerUI(mock_svgmapObj, false);
             console.log(svgmaplayerui);
+            closeButton = document.createElement("input");
+            closeButton.type = "button";
+            closeButton.value = "x";
+            jest.spyOn(document, "getElementById").mockReturnValue(closeButton);
         });
         it("ListMessageを登録する試験？", ()=>{
             svgmaplayerui.setLayerListmessage();
         });
         it("LayerSpecificUIを自動起動", ()=>{
-            svgmaplayerui.setLayerSpecificWebAppLaunchUiEnable();
+            let id = "0";
+            let controllerurl = "https://localhost/sample.html";
+            svgmaplayerui.setLayerSpecificWebAppLaunchUiEnable(id, controllerurl);
+            expect(closeButton.style.visibility).toBe("visible");
+            expect(closeButton.dataset.url).toBe(controllerurl);
         });
         it("LayerUIのカスタム設定の取得試験", ()=>{
             svgmaplayerui.getLayersCustomizer();
