54f1e10686769e4e4e49f27de79b6137b773755c
kusariya <yusaki.4kusari@gmail.com>
2024-09-12 11:54:33 +0900

    LayerListとLayerSpecificUIのカテゴリを分離

--- tests/SVGMapLv0.1_LayerUI_r6module.test.js
+++ tests/SVGMapLv0.1_LayerUI_r6module.test.js
@@ -3,28 +3,41 @@ import {mock_svgmapObj} from "../resources/mockParamerters"
 import {jest} from "@jest/globals";
 
 describe("unittest for LayerUI module",()=>{
-    describe("target svgMapImagesProps class",()=>{
+    //機能単位で試験分けておきます
+    describe("target LayerList",()=>{
         let svgmaplayerui, closeButton;
         beforeEach(()=>{
             svgmaplayerui = new SvgMapLayerUI(mock_svgmapObj, false);
-            console.log(svgmaplayerui);
+        });
+        it("ListMessageを登録する試験？", ()=>{
+            // UIの表示上の問題だけなので簡素な試験としてます
+            svgmaplayerui.setLayerListmessage("レイヤーリスト","レイヤー表示中");
+        });
+
+        it("test init function", ()=>{
+            // init関数はPrivate扱いですが、SVGMapObjectからコールされているためPublicのほうが適切では？
+        });
+
+        it("LayerUIのカスタム設定の取得試験", ()=>{
+            svgmaplayerui.getLayersCustomizer();
+        });
+    });
+    
+    describe("target LayerSpecificUI",()=>{
+        let svgmaplayerui, closeButton;
+        beforeEach(()=>{
+            svgmaplayerui = new SvgMapLayerUI(mock_svgmapObj, false);
             closeButton = document.createElement("input");
             closeButton.type = "button";
             closeButton.value = "x";
             jest.spyOn(document, "getElementById").mockReturnValue(closeButton);
         });
-        it("ListMessageを登録する試験？", ()=>{
-            svgmaplayerui.setLayerListmessage();
-        });
-        it("LayerSpecificUIを自動起動", ()=>{
+        it("LayerSpecificUI起動時のボタン群", ()=>{
             let id = "0";
             let controllerurl = "https://localhost/sample.html";
             svgmaplayerui.setLayerSpecificWebAppLaunchUiEnable(id, controllerurl);
             expect(closeButton.style.visibility).toBe("visible");
             expect(closeButton.dataset.url).toBe(controllerurl);
-        });
-        it("LayerUIのカスタム設定の取得試験", ()=>{
-            svgmaplayerui.getLayersCustomizer();
         });
     });
 });
