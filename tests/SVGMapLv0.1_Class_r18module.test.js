da581913899caa14f27a7aefb440d9581f96d067
kusariya <yusaki.4kusari@gmail.com>
2024-09-24 11:41:02 +0900

    mock化を外に出しました

--- tests/SVGMapLv0.1_Class_r18module.test.js
+++ tests/SVGMapLv0.1_Class_r18module.test.js
@@ -12,43 +12,37 @@ import {jest} from '@jest/globals';
 //      }
 //  });
 
+//================================================================
+// mocking 結構カオスになりそう
+//================================================================
+jest.unstable_mockModule('../libs/MapTicker.js', () => ({
+    MapTicker: jest.fn().mockImplementation(() => ({
+        constructor: jest.fn().mockReturnValue('Mocked Hello!'),
+    })),
+}));
+jest.unstable_mockModule('../libs/ZoomPanManager.js', () => ({
+    ZoomPanManager: jest.fn().mockImplementation(() => ({
+        constructor: jest.fn().mockReturnValue('Mocked Hello!'),
+        zoomup:jest.fn().mockReturnValue()
+    })),
+}));
+//================================================================
+
 describe("unittest for SVGMap Core Module", ()=>{
     
     describe("target SVGMap class",()=>{
         
         let svgmap, result, element;
         beforeEach(async () => {
-            //================================================================
-            // mocking 結構カオスになりそう
-            //================================================================
-            jest.unstable_mockModule('../libs/MapTicker.js', () => ({
-                MapTicker: jest.fn().mockImplementation(() => ({
-                    constructor: jest.fn().mockReturnValue('Mocked Hello!'),
-                })),
-            }));
-            jest.unstable_mockModule('../libs/MapViewerProps.js', () => ({
-                MapViewerProps: jest.fn().mockImplementation(() => ({
-                    constructor: jest.fn().mockReturnValue('Mocked Hello!'),
-                    uaProps:{
-                        verIE: 13,
-                    },
-                    hasUaProps: jest.fn().mockReturnValue(false),
-                    setCssTransform: jest.fn().mockReturnValue()
-                })),
-            }));
-            jest.unstable_mockModule('../libs/ZoomPanManager.js', () => ({
-                ZoomPanManager: jest.fn().mockImplementation(() => ({
-                    constructor: jest.fn().mockReturnValue('Mocked Hello!'),
-                    zoomup:jest.fn().mockReturnValue()
-                })),
-            }));
-            //================================================================
+            
             const {SvgMap} = await import("../SVGMapLv0.1_Class_r18module");
             svgmap = new SvgMap();
             svgmap.initLoad();
         });
 
         it("zoomUp", ()=>{
+            // ここではエラーがないこと程度しか確認してません。
+            // 詳細はコール先のzoompanmanagerにて確認すること
             svgmap.zoomup();
         });
     });
