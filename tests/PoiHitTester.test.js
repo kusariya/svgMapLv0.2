6d69a4606a4aabd001fdad6dd206013090e51dd5
kusariya <yusaki.4kusari@gmail.com>
2024-12-17 14:44:43 +0900

    apply prettier

--- tests/PoiHitTester.test.js
+++ tests/PoiHitTester.test.js
@@ -1,29 +1,27 @@
-import {PoiHitTester} from "../libs/PoiHitTester"
+import { PoiHitTester } from "../libs/PoiHitTester";
 
-describe("",()=>{
+describe("", () => {
 	let poi;
-	describe("",()=>{
-		
-		beforeAll(()=>{
+	describe("", () => {
+		beforeAll(() => {
 			poi = new PoiHitTester();
 		});
 
-		beforeEach(()=>{
-			for(let i = 0; i < 5; i++){
+		beforeEach(() => {
+			for (let i = 0; i < 5; i++) {
 				poi.setPoiBBox(i.toString(), 5, 3, 5, 8);
 			}
 		});
 
-		afterEach(()=>{
+		afterEach(() => {
 			poi.clear();
 		});
 
-		it("set POIs",()=>{
+		it("set POIs", () => {
 			let result = poi.getPoiObjectsAtPoint(7, 10); //データあり
 			expect(result).toHaveLength(5);
-			result = poi.getPoiObjectsAtPoint(100,100) //データなし
+			result = poi.getPoiObjectsAtPoint(100, 100); //データなし
 			expect(result).toHaveLength(0);
 		});
 	});
-
 });
